ffcbc055248c35d1ee2b50fcca8801c808f3c3dc
m-ohamed <mohamed.adel-ibrahim@student.guc.edu.eg>
2022-01-06 18:36:18 +0200

    adding comments for easier understandability

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -26,6 +26,10 @@ export class HomeComponent implements OnInit {
 
   constructor(private customerService: CustomerService) { }
 
+  /*
+  This function gets called when the page initializes. It currently calls the getAllCustomers() function which sends the GET request to the backend service
+  to provide it with the list of cusotmers. The list of customers is then set as the source for the Angular Material table (in MatTableDataSource).
+  */
   ngOnInit(): void {
     this.customerService.getAllCustomers().subscribe(
       (data) => {
@@ -35,10 +39,15 @@ export class HomeComponent implements OnInit {
     );
   }
 
+  //After the view is created this function is called. Its current role is to initialize the paginator.
   ngAfterViewInit() {
     this.customersSource.paginator = this.paginator;
   }
 
+  /*
+  Since the amount of data was not large, frontend filteration/search was used. This function is responsible to filter
+  the data received from the backend every time the search button is clicked.
+  */
   filterData() {
     var data = this.customersData.filter(customer => {
       if(this.selectedCountry != "" && this.selectedCountry != customer.country.toLowerCase())
